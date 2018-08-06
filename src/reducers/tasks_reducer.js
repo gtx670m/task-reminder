4f061b59a653dd05c790d6594e5dff4192a4686d
Quang Le <quang.le@infonam.com>
2018-08-06 13:28:44 +0700

    init

--- src/reducers/tasks_reducer.js
+++ src/reducers/tasks_reducer.js
@@ -20,11 +20,13 @@ var myReducer = (state = initialState, action) => {
             if (!newItem.id) {
                 newItem.id = randomString();
                 state.push(newItem);
+                alert('Added new Item!');
             } else {
                 index = state.findIndex((state) => {
                     return id === state.id;
                 });
                 state[index] = newItem;
+                alert('Saved!');
             }
             localStorage.setItem('tasks', JSON.stringify(state));
             return [...state];
